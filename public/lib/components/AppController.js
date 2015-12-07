6f6f10945e84ac204c4bf10f2340c682feaac00f
Damon Kalaboukis <damonkala@yahoo.com>
2015-12-07 13:47:04 -0800

    Done with just react

--- public/lib/components/AppController.js
+++ public/lib/components/AppController.js
@@ -1,16 +1,43 @@
 import React from "react";
+import $ from "jquery";
 import List from "./List";
+import Form from "./Form";
+import API from "./API"
 
+// Controller-View Component
 class AppController extends React.Component {
   constructor(props) {
     super(props);
     this.state = { bookmarks: [] };
   }
+  insertBookmark(newBookmark) {
+		API.saveBookmark(newBookmark)
+			 .done(data => {
+				 this.setState({
+					 bookmarks: this.state.bookmarks.concat(data)
+				 })
+			 })
+}
+  componentDidMount() {
+    console.log("did mount");
+    API.getAllBookmarks()
+	     .done(data => {
+        this.setState({ bookmarks: data.links });
+      });
+  }
+  componentWillMount() {
+    console.log("will mount");
+  }
+  componentWillUpdate() {
+    console.log("will update");
+  }
   render() {
+    console.log("render");
     return (
       <div className="app">
         <h2>Bookmarks!</h2>
         <List bookmarks={this.state.bookmarks} />
+        <Form addBookmark={this.insertBookmark.bind(this)} />
       </div>
     );
   }
